120a128455b3f48c0edc8a09233d70392f1ac773
paintt23 <titinan.loawkul@gmail.com>
2023-08-29 16:17:23 +0700

    step5 header:finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.scss'
+import { FaHome } from 'react-icons/fa';
 function App() {
   return (
     <div className='todo'>
-        <div className="todo__header">Header</div>
+        <div className="todo__header">
+   <header className='header'>
+
+    {/* Logo */}
+    <div className='header__logo'><FaHome/></div>
+
+    {/* Text */}
+    <div className='header__text'>
+        <h1>Todoist</h1>
+    </div>
+
+    {/* Search */}
+    <div className='header__search'>
+      <input/>
+    </div>
+</header>
+</div>
+
         <div className="todo__sidebar">SideBar</div>
-        <div className="todo_content">TodoContent</div>
+        <div className="todo__content">TodoContent</div>
     </div>
   );
 }
