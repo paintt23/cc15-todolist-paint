a8b930a45d5408d67380d338f4aa7eecb651e7d1
paintt23 <titinan.loawkul@gmail.com>
2023-08-29 15:21:04 +0700

    step4:App laout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-
-
+import './App.scss'
 function App() {
   return (
-    <div >
-   react App
+    <div className='todo'>
+        <div className="todo__header">Header</div>
+        <div className="todo__sidebar">SideBar</div>
+        <div className="todo_content">TodoContent</div>
     </div>
   );
 }
